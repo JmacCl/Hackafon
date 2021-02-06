d07e669c1957fa97b7c444f34572cdab5fcd9a4d
Ben Brown <9870007+brownben@users.noreply.github.com>
2021-02-06 20:35:19 +0000

    fix: remove bold message from sample

--- SAS/goals/views.py
+++ SAS/goals/views.py
@@ -8,53 +8,52 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    
-    context_dict = {'boldmessage':'EXAMPLE' }
-    
+    context_dict = {}
+
     try:
         context_dict['profile']=UserProfile.objects.get(user=request.user)
     except:
         context_dict['profile']="ono"
-    
+
     return render(request, 'goals/index.html', context=context_dict)
-    
+
 def register(request):
     registered = False
-    
+
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
-        
+
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            
+
             profile = profile_form.save(commit=False)
             profile.user = user
-            
-            
+
+
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
-                
+
             profile.save()
             registered = True
-            
+
         else:
             print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
-        
+
     return render(request, 'goals/register.html', context = {'user_form':user_form, 'profile_form': profile_form, 'registered': registered})
-    
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
         user = authenticate(username=username, password=password)
-        
+
         if user:
             if user.is_active:
                 login(request, user)
@@ -66,7 +65,7 @@ def user_login(request):
              return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'goals/login.html')
-        
+
 @login_required
 def user_logout(request):
     logout(request)
