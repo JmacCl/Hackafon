57798762f902d9078fdd633a99bca0d87155ece4
brownben <9870007+brownben@users.noreply.github.com>
2021-02-06 22:18:58 +0000

    move inspirational quotes to python file

---
+++ SAS/goals/inspirationalQuotes.py
@@ -0,0 +1,127 @@
+quotes = [
+    {"quote": "Live your dreams.", "author": "Les Brown"},
+    {
+        "quote": "The true vocation of man is to find his way for himself.",
+        "author": "Hermann Hesse",
+    },
+    {
+        "quote": "A life of sacrifice is the pinnacle of art, and is full of true joy.",
+        "author": "Mahatma Gandhi",
+    },
+    {"quote": "No wind favors he who has no destined port.", "author": "Montaigne"},
+    {
+        "quote": "It’s not what you look at that matters, it’s what you see.",
+        "author": "Henry Davis Thoreau",
+    },
+    {
+        "quote": "Only a life lived for others is a life worthwhile.",
+        "author": "Albert Einstein",
+    },
+    {"quote": "To know oneself, one should assert oneself.", "author": "Albert Camus"},
+    {
+        "quote": "Men do less than they ought unless they do all they can.",
+        "author": "Thomas Carlyle",
+    },
+    {"quote": "People living deeply have no fear of death.", "author": "Anais Nin"},
+    {
+        "quote": "A clever man commits no minor blunders.",
+        "author": "Johann Wolfgang von Goethe",
+    },
+    {
+        "quote": "Be not afraid of going slowly, be afraid only of standing still.",
+        "author": "Chinese Proverb",
+    },
+    {
+        "quote": "If I’m gonna tell a real story, I’m gonna start with my name.",
+        "author": "Kendrick Lamar",
+    },
+    {
+        "quote": "Wake up with determination. Go to bed with satisfaction.",
+        "author": "George Horace Lorimer",
+    },
+    {"quote": "All limitations are self-imposed.", "author": "Oliver Wendell Holmes"},
+    {
+        "quote": "When you make a choice, you change the future.",
+        "author": "Deepak Chopra",
+    },
+    {
+        "quote": "Determine your priorities and focus on them.",
+        "author": "Eileen McDargh",
+    },
+    {
+        "quote": "Words are your paintbrush, and your life is the canvas. You can paint whatever you want to paint.",
+        "author": "Miguel Ruiz",
+    },
+    {
+        "quote": "Never let your emotions overpower your intelligence.",
+        "author": "Drake",
+    },
+    {
+        "quote": "Luck is a matter of preparation meeting opportunity.",
+        "author": "Oprah Winfrey",
+    },
+    {"quote": "Let the beauty of what you love be what you do.", "author": "Rumi"},
+    {
+        "quote": "Always remember that you are unique  –  just like everybody else.",
+        "author": "Unknown",
+    },
+    {
+        "quote": "If you tell the truth you don’t have to remember anything.",
+        "author": "Mark Twain",
+    },
+    {
+        "quote": "Re-examine all you have been told. Dismiss what insults your soul.",
+        "author": "Walt Whitman",
+    },
+    {
+        "quote": "Have enough courage to start and enough heart to finish.",
+        "author": "Jessica N. S. Yourko",
+    },
+    {"quote": "Above all, a person is their will.", "author": "Unknown"},
+    {"quote": "Be so good they can’t ignore you.", "author": "Steve Martin"},
+    {"quote": "If opportunity doesn’t knock, build a door.", "author": "Milton Berle"},
+    {
+        "quote": "Don’t you know your imperfections is a blessing?",
+        "author": "Kendrick Lamar",
+    },
+    {
+        "quote": "Who you are tomorrow begins with what you do today.",
+        "author": "Tim Fargo",
+    },
+    {"quote": "Happiness depends upon ourselves.", "author": "Aristotle"},
+    {
+        "quote": "You can’t have everything. Where would you put it?",
+        "author": "Steven Wright",
+    },
+    {
+        "quote": "Tough times never last but tough people do.",
+        "author": "Robert H. Schiuller",
+    },
+    {
+        "quote": "Magic always happens when you direct your inner powers to the object you want to change.",
+        "author": "Bangambiki Habyarimana",
+    },
+    {
+        "quote": "I could agree with you but then we’d both be wrong.",
+        "author": "Harvey Specter",
+    },
+    {
+        "quote": "The only impossible journey is the one you never begin.",
+        "author": "Tony Robbins",
+    },
+    {"quote": "Yesterday you said tomorrow. Just do it.", "author": "Nike"},
+    {
+        "quote": "Those at the top of the mountain didn’t fall there.",
+        "author": "Marcus Washling",
+    },
+    {"quote": "There is no substitute for hard work.", "author": "Thomas Edison"},
+    {
+        "quote": "Mostly, the world sees you the way you see yourself.",
+        "author": "Lisa Nichols",
+    },
+    {"quote": "The meaning of life is to give life meaning.", "author": "Ken Hudgins"},
+    {
+        "quote": "The elevator to success is out of order. You’ll have to use the stairs, one step at a time.",
+        "author": "Joe Girard",
+    },
+]
