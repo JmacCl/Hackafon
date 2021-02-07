7489bac7dd82715b837f7417ece41b6a86a9698d
Alistair Hampton <2459255H'student.gla.ac.uk>
2021-02-07 00:02:51 +0000

    Fixed Profile Picture Uploading

--- SAS/goals/models.py
+++ SAS/goals/models.py
@@ -7,14 +7,6 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='profile_images', blank=True)
     xpLevel = models.IntegerField(default=1)
 
-    def save(self, *args, **kwargs):
-        split_file_name = self.picture.name.split('.')
-        new_name = self.user.username
-        if len(split_file_name) > 0:
-            new_name += '.' + split_file_name[1]
-        self.picture.name = new_name
-        super(UserProfile, self).save(*args, **kwargs)
-
     def __str__(self):
         return self.user.username
 
